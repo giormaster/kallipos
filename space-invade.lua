84159cc3bf85e7c118a194b27ea94516ec27d137
George L <p18lomv@ionio.gr>
2022-11-29 20:42:19 +0200

    Update space-invade.lua

--- space-invade.lua
+++ space-invade.lua
@@ -3,9 +3,9 @@ function Image(img)
 		local f = io.open("Mycontribution/" .. img.src, 'r')
 		local doc = pandoc.read(f:read('*a'))
 		f:close()
-		local caption = pandoc.utils.stringify(doc.meta.caption)
 		local name = pandoc.utils.stringify(doc.meta.name)
 		local am = pandoc.utils.stringify(doc.meta.id)
+		local caption = pandoc.utils.stringify(doc.meta.caption)
 		local content = "> " .. caption .. " \n>>" .. "Student Fullname:" .. name .. "\nStudent Identification Number:" ..am
 		return pandoc.RawInline('markdown',content)
 	end
